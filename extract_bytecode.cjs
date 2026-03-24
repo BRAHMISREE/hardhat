5d334a1cd9103f97909f7081ded85f2ee518b565
K.Brahmi sree <brahmisree786@gmail.com>
2026-03-24 20:39:02 +0530

    compiled other 100 og codes

--- extract_bytecode.cjs
+++ extract_bytecode.cjs
@@ -2,7 +2,7 @@ const fs = require("fs");
 const path = require("path");
 
 const artifactsPath = "./artifacts/contracts";
-const outputFile = "obfuscated_bytecodes.csv";
+const outputFile = "bytecodes.csv";
 
 if (!fs.existsSync(outputFile)) {
     fs.writeFileSync(outputFile, "file_name,contract_name,bytecode\n");
--- obfuscated/batch01_contract_196.sol
+++
@@ -1,8 +0,0 @@
-Contract Name:
-RTB2
-Compiler Version
-v0.4.23+commit.124ca40d
-Optimization Enabled:
-Yes with 200 runs
-Other Settings:
-default evmVersion
